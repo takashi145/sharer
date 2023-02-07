8fcc99c38ed98054e7e533bdd2b59c9027686a5d
takashi145 <t.shuhei0203@gmail.com>
2023-02-07 13:41:52 +0900

    post create

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StorePostRequest;
 use App\Http\Resources\PostResource;
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 
 class PostController extends Controller
@@ -14,9 +16,34 @@ class PostController extends Controller
      */
     public function index()
     {
-        $posts = Post::with('user')->get();
+        $posts = Post::with('user')->where('published', '=', 1)->get();
         return Inertia::render('Post/Index', [
             'posts' => $posts
         ]);
+    }
+
+    /**
+     * 投稿作成画面を表示
+     */
+    public function create()
+    {
+        return Inertia::render('Post/Create');
+    }
+
+    /**
+     * 投稿を保存
+     * 
+     * @param App\Http\Requests\StorePostRequest $request
+     */
+    public function store(StorePostRequest $request)
+    {
+        Post::create([
+            'user_id' => Auth::id(),
+            'title' => $request->input('title'),
+            'description' => $request->input('description'),
+            'published' => 0
+        ]);
+
+        return to_route('post.index');
     }
 }
---
+++ resources/js/Pages/Post/Create.vue
@@ -0,0 +1,42 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head, Link, useForm } from '@inertiajs/vue3';
+import { defineProps } from 'vue';
+import InputError from '@/Components/InputError.vue';
+
+defineProps({
+  errors: Object
+})
+
+const form = useForm({
+  'title': '',
+  'description': '',
+});
+
+const submit = () => {
+  form.post('/post', {
+    preserveScroll: true,
+    onSuccess: () => form.reset(),
+  });
+}
+</script>
+
+<template>
+    <Head title="CreatePost" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">PostList</h2>
+        </template>
+
+        <div class="py-12">
+            <form @submit.prevent="submit" class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <input type="text" v-model="form.title" class="border">
+                <InputError :message="errors.title" /><br />
+                <input type="text" v-model="form.description" class="border">
+                <InputError :message="errors.description" />
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    </AuthenticatedLayout>
+</template>
